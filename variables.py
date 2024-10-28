fc235576e1f4e53a7510962d6a77ce3779971704
Daleto <daleto24@gmail.com>
2024-10-28 14:19:57 -0500

    Version 1.0.1

--- variables.py
+++ variables.py
@@ -38,3 +38,14 @@ print('Last name: ', last_name)
 print('Country: ', country)
 print('Age: ', age)
 print('Married: ', is_married)
+
+# Declaring multiple variables in one line
+
+first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+
+print(first_name, last_name, country, age, is_married)
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
